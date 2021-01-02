51e7b6ca92dc402dff7a116a615e18c61f08398f
onesword0618 <ao.akua.leo@gmail.com>
2021-01-02 20:13:34 +0900

    Add: webpackの設定を追加した

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+let debug   = process.env.NODE_ENV !== "production";
+let webpack = require('webpack');
+let path    = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./index.js",
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }]
+      }]
+    },
+    output: {
+      path: __dirname + "/dist/",
+      filename: "build.js"
+    },
+    plugins: debug ? [] : [
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ]
+};
