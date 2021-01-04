debae4c81c9c65f914e024dad59dfbc6458f5e38
onesword0618 <ao.akua.leo@gmail.com>
2021-01-04 10:09:21 +0900

    Update: クリック時にアラートが出力するように実装した
    クリック時にアラートが出るようにした
    https://reactjs.org/tutorial/tutorial.html#making-an-interactive-component
    のnoteで出力するようにした。

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
