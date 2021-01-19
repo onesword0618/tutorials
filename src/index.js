e0da398b6b38bdcdcd38ee0ebb60439765381510
onesword0618 <ao.akua.leo@gmail.com>
2021-01-20 08:53:44 +0900

    Update: 切り出したBoardコンポーネントを外部から呼び出すように変更した

--- src/index.js
+++ src/index.js
@@ -3,39 +3,8 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { calculateWinner } from './domain/decide';
-import { Square } from './components/Square';
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
+import { Board } from './components/Board';
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
 class Game extends React.Component {
   constructor(props) {
     super(props);
