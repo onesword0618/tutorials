4f9d360ecaea314e24b1f1ee55331b37759ab595
onesword0618 <ao.akua.leo@gmail.com>
2021-01-20 07:10:57 +0900

    Add: Squareのコンポーネントを別ファイルに切り出した

---
+++ test/components/Square.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Square} from '../../src/components/Square';
+import renderer from 'react-test-renderer';
+
+describe('マス目を表現しているコンポーネントの検証',() => {
+
+  describe('マス目が描画されること',() => {
+    test('SnapShotと同一であること',() => {
+      const sut = renderer.create(
+        <Square
+          value = {'O'}
+          onClick = {() => 1}
+        />
+      );
+
+      let target =sut.toJSON();
+      expect(target).toMatchSnapshot();
+    });
+  });
+});
