83d6cb44fd12514ce3c998ab322df1f12de3ff48
onesword0618 <ao.akua.leo@gmail.com>
2021-02-04 22:03:10 +0900

    Update: 実行履歴のコンポーネントにマスの位置情報を追記した
    位置情報を取得して実行履歴の一覧に表記するように変更した

--- src/components/Moves.tsx
+++ src/components/Moves.tsx
@@ -1,6 +1,6 @@
 import '../index.css';
-import {History} from '../domain/type';
 
+import {History} from '../domain/type';
 import React from 'react';
 
 type Props = {
@@ -12,11 +12,12 @@ export const Moves : React.FC<Props> = (props) => {
   const {history,jumpTo} = props;
   return (
     <ol>
-    {history.map((_, move) => {
-      const desc = move ? 'Go to move #' + move : 'Go to game start';
+    {history.map((step, move) => {
+      const desc = move ? `Go to move #${move}.` : 'Go to game start';
+      const location = move ? `(${step.location.col}, ${step.location.row})` : '';
       return (
         <li key={move}>
-          <button onClick={() => jumpTo(move)}>{desc}</button>
+          <button onClick={() => jumpTo(move)}>{desc} {location}</button>
         </li>
       );
     })}
