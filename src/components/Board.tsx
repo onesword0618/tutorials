ee8ab4a75e1d8cdeacb3ef8e5dc558736268ae2c
onesword0618 <ao.akua.leo@gmail.com>
2021-02-13 20:07:44 +0900

    Update: ループ処理の再設計を実施した
    ・描画する行数を確定させる。
    ・行を描画する際に、確定した列数を描画する。
    ・描画する際に、squareコンポーネントを差し込む。
    
    行単位でループしながら列を描画していく。

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,35 +6,40 @@ import { Square } from '../components/Square';
 type Props = {
   onClick:(i:number) => void,
   squares:('X' | 'O'| null)[]
-}
+};
 
 export const Board : React.FC<Props> = (props) => {
   const {onClick,squares} = props;
   function renderSquare(i:number) {
     return (
       <Square
+        key={i}
         onClick={() => onClick(i)}
         value={squares[i]}
       />
     );
-  }
-    return (
-      <div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
+  };
+
+  const rows = Array(3);
+  for(let i of [0,1,2]) {
+
+    const columns = Array(3);
+    for (let j of [0,1,2]) {
+      const index = i*3 + j;
+      columns[j] = renderSquare(index);
+    };
+
+    rows[i] = (
+      <div key={i} className='board-row'>
+        {columns}
       </div>
     );
-}
+
+  };
+
+    return (
+      <div>
+        {rows}
+      </div>
+    );
+};
