eaa1b52c303e37b5a7c6f8116341f63f8c39e1d4
onesword0618 <ao.akua.leo@gmail.com>
2021-02-16 22:29:37 +0900

    Update: 実行履歴の修正を行った
    <ol>をGameコンポーネントに渡した。
    toggleボタンの動きと連動させるため

--- src/components/Moves.tsx
+++ src/components/Moves.tsx
@@ -12,19 +12,19 @@ type Props = {
 export const Moves : React.FC<Props> = (props) => {
   const {history,jumpTo,currentNumber} = props;
   return (
-    <ol>
-    {history.map((step, move) => {
-      const desc = move ? `Go to move #${move}.` : 'Go to game start';
-      const location = move ? `(${step.location.col}, ${step.location.row})` : '';
-      return (
-        <li key={move}>
-          <button onClick={() => jumpTo(move)}>
-            {move == currentNumber ? <b>{desc}</b> : desc}
-            {location}
-          </button>
-        </li>
-      );
-    })}
-  </ol>
+    <>
+      {history.map((step, move) => {
+        const desc = move ? `Go to move #${move}.` : 'Go to game start';
+        const location = move ? `(${step.location.col}, ${step.location.row})` : '';
+        return (
+          <li key={move}>
+            <button onClick={() => jumpTo(move)}>
+              {move == currentNumber ? <b>{desc}</b> : desc}
+              {location}
+            </button>
+          </li>
+        );
+      })}
+    </>
   );
 }
