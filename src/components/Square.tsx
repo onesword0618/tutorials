3544a430da3a441069441e300260e01e6ee7ca0c
onesword0618 <ao.akua.leo@gmail.com>
2021-02-19 01:26:08 +0900

    Update: ハイライトできるようにタグの名前を修正した
    外から渡されたパラメータを元にクラス名を変更するように修正した

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -5,13 +5,20 @@ import React from 'react';
 type Props = {
   onClick:() => void,
   value: 'X' | 'O'| null,
+  highlight:(boolean|null),
 }
 
 export const Square: React.FC<Props> = (props) => {
   const {onClick,value} = props;
+
+  let className = 'square';
+  if(props.highlight) {
+    className += ' highlight'
+  }
+
   return (
     <button
-      className="square"
+      className={className}
       onClick={onClick}
     >
       {value}
