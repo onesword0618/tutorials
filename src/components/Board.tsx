0fea981a1d40f8574ee905c2799a5592e37135fa
onesword0618 <ao.akua.leo@gmail.com>
2021-02-19 01:24:46 +0900

    Update: squareにパラメータを渡すように修正した
    描画するsquareにハイライトするようにパラメータを渡した。

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,17 +5,19 @@ import { Square } from '../components/Square';
 
 type Props = {
   onClick:(i:number) => void,
-  squares:('X' | 'O'| null)[]
+  squares:('X' | 'O'| null)[],
+  line:(number[] | null),
 };
 
 export const Board : React.FC<Props> = (props) => {
-  const {onClick,squares} = props;
-  function renderSquare(i:number) {
+  const {onClick,squares,line} = props;
+  function renderSquare(i:number,highlight:(boolean|null)) {
     return (
       <Square
         key={i}
         onClick={() => onClick(i)}
         value={squares[i]}
+        highlight={highlight}
       />
     );
   };
@@ -26,7 +28,8 @@ export const Board : React.FC<Props> = (props) => {
     const columns = Array(3);
     for (let j of [0,1,2]) {
       const index = i*3 + j;
-      columns[j] = renderSquare(index);
+      const highlight = line && line.includes(index);
+      columns[j] = renderSquare(index,highlight);
     };
 
     rows[i] = (
